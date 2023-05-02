107a0d53d364c3913848448c8742fefd24919d36
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2023-05-02 20:06:50 +1000

    Initial commit

---
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.news;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton news1,news2,news3,news4;
+    RecyclerView HorizontalRecycler,VerticalRecycler;
+    ArrayList<String> dataSource;
+    LinearLayoutManager linearLayoutManager;
+    MyAdapter myAdapter;
+    FragmentContainerView fragment_Container_View;
+    private ImageView inflation,wed,child,law;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        news1=findViewById(R.id.News1);
+        news2=findViewById(R.id.News2);
+        news3=findViewById(R.id.News3);
+        news4=findViewById(R.id.News4);
+
+        fragment_Container_View=findViewById(R.id.fragmentContainerView);
+
+        HorizontalRecycler=findViewById(R.id.horizontal_recycler_view);
+
+        dataSource=new ArrayList<>();
+        dataSource.add("wedding_trends_png.webp");
+        dataSource.add("rsz_1gettyimages_157311703_b09b9b53d83d4f35bcbedf9ffb6c8a8b.jpg");
+        dataSource.add("erferfef.jpg");
+        dataSource.add("dwedwedwd.jpg");
+
+        linearLayoutManager=new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        myAdapter=new MyAdapter(dataSource);
+        HorizontalRecycler.setLayoutManager(linearLayoutManager);
+        HorizontalRecycler.setAdapter(myAdapter);
+
+        fragment_Container_View.setVisibility(View.INVISIBLE);
+
+        news1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment_Container_View.setVisibility(View.VISIBLE);
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView,FirstNewsFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("FirstNews")
+                        .commit();
+            }
+        });
+
+
+
+        news2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment_Container_View.setVisibility(View.VISIBLE);
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.fragmentContainerView,SecondNewsFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("SecondNews")
+                        .commit();
+            }
+        });
+
+        news3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment_Container_View.setVisibility(View.VISIBLE);
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.fragmentContainerView,ThirdNewsFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("ThirdNews")
+                        .commit();
+            }
+        });
+
+        news4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment_Container_View.setVisibility(View.VISIBLE);
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.fragmentContainerView,FourthNewsFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("FourthNews")
+                        .commit();
+            }
+        });
+
+    }
+
+    public void handleImageButtonClick() {
+        fragment_Container_View.setVisibility(View.INVISIBLE);
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder>
+    {
+        ArrayList<String> dataSource;
+        public MyAdapter(ArrayList<String> data) {
+            this.dataSource=data;
+        }
+
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view= LayoutInflater.from(MainActivity.this).inflate(R.layout.topstories,parent,false);
+            return new MyHolder(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.size();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+//            holder.imageView.setImageDrawable(Drawable.createFromPath(dataSource.get(position)));
+        }
+
+        class MyHolder extends RecyclerView.ViewHolder
+        {
+            ImageView imageView;
+            public MyHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView=findViewById(R.id.Inflation);
+            }
+        }
+    }
+
+}
